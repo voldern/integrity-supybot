41b468cac2f30bd427b83df8453c3ec6297c1fac
Espen Volden <espenv@hardware.no>
2009-04-23 17:11:57 +0200

    Fixed endless loop. short_message called short_message

--- lib/notifier/integrity_supybot.rb
+++ lib/notifier/integrity_supybot.rb
@@ -13,14 +13,14 @@ module Integrity
       def deliver!
         my = Mysql.new(@config['host'], @config['user'], @config['pass'], @config['db'])
         st = my.prepare('INSERT INTO message_queue SET user = ?, created = NOW(), message= ?')
-        st.execute(@config['name'], short_message)
+        st.execute(@config['name'], irc_message)
         st.close
         my.close
       end
 
       private
 
-      def short_message
+      def irc_message
         "#{build.project.name}: #{short_message}"
       end
 
